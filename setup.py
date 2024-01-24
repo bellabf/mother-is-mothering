e29506e40f44646dc45c98878fc10168e4fde471
Isabella Bicaho-Frazeto <bicalhoisabella@gmail.com>
2024-01-24 19:00:12 +0100

    toml again

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 import os
-import glob
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 
@@ -31,10 +30,12 @@ setup(
     description=description,
     long_description='Longer package description goes here.',
     packages=find_packages(),
-    install_requires=["numpy>=1.26.3"], 
-    extras_require={},   
+    include_package_data=True,
+    install_requires=["numpy>=1.26.3"],
+    package_data={
+        'mother-is-mothering': ['src/ts-lyrics.pkl'],
+    },
+    extras_require={},
     python_requires='>=3.7',
     zip_safe=False,
-    cmdclass={'bdist_wheel': 'bdist_wheel'},
-    include_data_files=[(d, [f]) for d, _, fs in os.walk('.') for f in fs if f.endswith('.pkl')],
 )
